51159e969e426634cd723b8fda7a07d33d23376d
LAPTOP-APB1FLL2\Jose Antonio <jose.silva.a@mail.pucv.cl>
2023-03-11 17:08:18 -0300

    Change: add store provider to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import TodoResults from "./components/TodoResults";
+import { Provider } from "react-redux";
+import {store} from "./app/store";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="root">
-      <TodoList />
-      <TodoResults />
+      <Provider store={store}>
+        <TodoList />
+        <TodoResults />
+      </Provider>
+      
     </div>
   );
 };
