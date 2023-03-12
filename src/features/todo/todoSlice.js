125747214551c09b19e4bcc567f4ef44b15834c0
LAPTOP-APB1FLL2\Jose Antonio <jose.silva.a@mail.pucv.cl>
2023-03-11 22:03:02 -0300

    Change: add data list to reducer

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,10 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice ,createAsyncThunk} from '@reduxjs/toolkit'
 
+export const getTodoList = createAsyncThunk('todos/getTodoList',async()=>{
+    const response = await fetch('https://my-json-server.typicode.com/AlvaroArratia/static-todos-api/todos') ;
+    return response.data ;
+})
 export const todoSlice = createSlice({
-    name:'todo',
-    initialState:[],
+    name:'todos',
+    initialState:[
+        {
+            "id": 0,
+            "label": "Fix an ability to display all tasks",
+            "checked": false
+        },
+        {
+            "id": 1,
+            "label": "Fix a layout, checkboxes should be listed in a column",
+            "checked": false
+        },
+        {
+            "id": 2,
+            "label": "Fix an ability to add a new task",
+            "checked": false
+        },
+        {
+            "id": 3,
+            "label": "Fix an ability to toggle a task",
+            "checked": false
+        },
+        {
+            "id": 4,
+            "label": "Fix an ability to delete a task",
+            "checked": false
+        },
+        {
+            "id": 5,
+            "label": "Fix an ability to count completed tasks",
+            "checked": false
+        }
+    ],
     reducers: {}
 });
-
+export default todoSlice.reducer ;
 
 
