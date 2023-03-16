7b706502176fec9ade3eac098f9c497f8e80aa58
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-03-16 18:10:36 -0300

    Change: add progress bar of todos done

--- src/components/TodoResults/index.jsx
+++ src/components/TodoResults/index.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import "./styles.css";
 
 const TodoResults = () => {
   // Fix an ability to calculate completed tasks
 
-  return <div className="todo-results">Done:</div>;
+const selector = useSelector( state=>state.todos);
+const total = selector.length ;
+let doneCounter = 0 ;
+ for(let todo of selector){
+  if(todo.checked){
+    doneCounter++;
+  }
+ };
+ let percentage = (doneCounter / total) * 100 ;
+ if(percentage < 100) percentage = Math.trunc(percentage);
+ 
+
+
+  return (
+    <div >
+      <div className="todo-results">
+        <h3>Done:{doneCounter}</h3>
+
+      </div>
+      <h4>Percentage done: </h4>
+      <div className="progressContainer">
+      
+      <div
+        className="progress-bar-fill"
+        style={{ width: `${percentage}%` }}
+      >{percentage}%</div>
+      </div>
+    </div>
+    
+  )
+  
 };
 
 export default TodoResults;
