87fe97eea088bb10d62c9e215ea61f0cd8df2678
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-03-16 18:44:45 -0300

    FIx: just add todos with text

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -5,7 +5,7 @@ import './styles.css';
 import {v4} from 'uuid';
 
 const TodoForm = () =>{
-    const [label,setLabel]= React.useState();
+    const [label,setLabel]= React.useState('');
     const dispatch = useDispatch();
     const handleChange=(event)=>{
       
@@ -14,12 +14,18 @@ const TodoForm = () =>{
     };
     const handleSubmit=(event)=>{
         event.preventDefault();
+        const inputValue = label.trim(); // obtener el valor del input y eliminar espacios en blanco al inicio y al final
+  
+        if (!inputValue) {
+          // Si el valor del input está vacío, no hacer nada
+          return;
+        }
         dispatch(addTodo({
-            label,
+            label: inputValue,
             id: v4(),
             checked: false
         }));
-        document.getElementById('inputTodo').value = null;
+        setLabel('');
     };
 
 
@@ -30,11 +36,11 @@ const TodoForm = () =>{
                 placeholder=" Enter new to do"
                 className="todoInput"
                 type="text"
+                value={label}
                 onChange={handleChange}
-            
                 autoComplete="false"
                 />
-                <button onClick={handleSubmit}className="btn btn-primary btn-add" type="Submit" > ADD TO DO </button>
+                <button onClick={handleSubmit}className="btn  btn-primary btn-add" type="Submit" > ADD TO DO </button>
         </form>
         
     )
