34a9843047383a42dc24582b7c8abf5f07f52a2c
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-03-17 01:11:24 -0300

    Fix: unused var

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import TodoResults from "./components/TodoResults";
-import {useSelector} from 'react-redux';
 
 import "./App.css";
 import TodoForm from "components/TodoForm";
